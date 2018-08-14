d26f401979d9d481b7829089459623fdfd22ec53
zhangshuang-h <781792980@qq.com>
2018-08-14 21:52:28 +0800

    my home.vue

---
+++ vuecli3/src/views/myHome.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>
+            welcome
+        </h1>
+        <router-link :to="{name:'test1',params:{name:'zhangsan',age:13}}">子路由1</router-link> |
+        <router-link to="/myhome/test2/lisi/15">子路由2</router-link>
+        <router-view></router-view>
+    </div>
+
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
