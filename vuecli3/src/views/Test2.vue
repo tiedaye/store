d51cb6731d5024e6cb3ab402ad73c6b31aaa6e72
zhangshuang-h <781792980@qq.com>
2018-08-14 21:54:36 +0800

    test2

---
+++ vuecli3/src/views/Test2.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>子路由test2</h1>
+        名字是：{{$route.params.xx}},
+        年龄是：{{$route.params.age}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Test2"
+    }
+</script>
+
+<style scoped>
+
+</style>
