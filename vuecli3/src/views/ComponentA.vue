5e8d45e074d44d0af9013987e416464977abfd64
zhangshuang-h <781792980@qq.com>
2018-08-15 23:09:32 +0800

    componenta

---
+++ vuecli3/src/views/ComponentA.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>我是componentA页面</h1>
+        <component-b :msg="xx"></component-b>
+        <component-c @changed="changed"></component-c>
+        {{xx}}
+    </div>
+</template>
+<script>
+    import ComponentB from '@/views/ComponentB.vue';
+    import ComponentC from './ComponentC.vue';
+    export default {
+            data(){
+                return{
+                    xx:0
+                }
+            },
+            components:{
+                ComponentB,
+                ComponentC
+            },
+            methods:{
+                changed(a){
+                    this.xx +=a;
+                }
+            }
+
+    }
+</script>
+<style>
+
+</style>
