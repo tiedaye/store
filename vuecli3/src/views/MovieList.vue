fb4177e46d7f3960c48936572501e2e182293f73
zhangshuang-h <781792980@qq.com>
2018-08-15 23:11:19 +0800

    movielist

---
+++ vuecli3/src/views/MovieList.vue
@@ -0,0 +1,42 @@
+<template>
+    <ul>
+        <li v-for="val in list" @click="click(val.id)">
+            <!--<router-link :to="'/moviedetail/'+val.id">-->
+                {{val.name}},{{val.grade}}
+            <!--</router-link>-->
+        </li>
+    </ul>
+</template>
+<script>
+    export  default {
+        data(){
+            return{
+                    list:[
+                            {
+                                name:'西红柿首富',
+                                grade:8.7,
+                                id:111
+                            },
+                            {
+                                name:'爱情公寓',
+                                grade:6.0,
+                                id:222
+                            },
+                            {
+                                name:'巨齿鲨',
+                                grade:9.7,
+                                id:333
+                            }
+                        ]
+                    }
+             },
+        methods:{
+            click(id){
+                return this.$router.push('/moviedetail/'+id);
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
