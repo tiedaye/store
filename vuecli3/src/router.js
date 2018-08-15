6f2b7f9e0e013a9bf2ee8f4fd915c3dd3a29720a
zhangshuang-h <781792980@qq.com>
2018-08-15 23:12:16 +0800

    route

--- vuecli3/src/router.js
+++ vuecli3/src/router.js
@@ -5,57 +5,105 @@ import Mine from './views/Mine.vue';
 import myHome from './views/myHome.vue';
 import Test1 from './views/Test1.vue';
 import Test2 from './views/Test2.vue';
-Vue.use(Router);
+import Error from './views/Error.vue';
+import Movie from './views/MovieList.vue';
+import MovieDetail from './views/MovieDetail.vue';
+import ComponentA from './views/ComponentA.vue';
+import Vuex from './views/Vuex.vue'
 
-export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-    },
-    {
-        path: '/about',
-        name: 'about',
+ Vue.use(Router);
 
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+ let router = new Router({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home,
+        },
+        {
+            path: '/about/:xx',
+            name: 'about',
 
-    },
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
 
-    {
-        path: '/mine',
-        component: Mine
-    },
-      {
-          path: '/sssss',
-          redirect:'/'
-      },
-      {
-          path: '/sssss/:xx/:age',
-          redirect:'/myhome/test2/:xx/:age'
-      },
-      {
-          path: '/myhome',
-          component: myHome,
-          children:[
-              {
-                //写app中不写/  单独写myhome中要写/
-                  path: 'test1',
-                  name:'test1',
-                  component: Test1,
-                  alias:'/xiaoming',
-              },
-              {
-                  path: 'test2/:xx/:age',
-                  component: Test2,
-              }
-          ]
-      },
+        },
 
-  ],
+        {
+            path: '/mine',
+            component: Mine
+        },
+        {
+            path: '*',
+            component: Error
+        },
+        {
+            path: '/sssss',
+            redirect:'/'
+        },
+        {
+            path: '/sssss/:xx/:age',
+            redirect:'/myhome/test2/:xx/:age'
+        },
+        {
+            path: '/movie',
+            component: Movie,
+        },
+        {
+            path: '/moviedetail/:id',
+            component: MovieDetail,
+        },
+        {
+            path: '/componenta',
+            component: ComponentA,
+        },
+        {
+            path: '/vuex',
+            component: Vuex,
+        },
+        {
+            path: '/myhome',
+            component: myHome,
+            children:[
+                {
+                    //写app中不写/  单独写myhome中要写/
+                    path: 'test1',
+                    name:'test1',
+                    component: Test1,
+                    alias:'/xiaoming',
+                },
+                {
+                    path: 'test2/:xx/:age',
+                    component: Test2,
+                }
+            ]
+        },
+
+    ],
 });
+ router.beforeEach((to,from,next)=>{
+     // console.log(to);
+     // console.log(from);
+     // if(from.name=='about'){
+     //     next(false);
+     //     console.log(from.name=='about')
+     // }else{
+     //     next();
+     // }
+     // if(to.name=='home'){
+     //     next(false);
+     //     console.log('不能跳转2');
+     // }else{
+     //     next();
+     // }  //影响
+     // next(false)  //不允许向下执行
+     next();
+ });
+router.afterEach(()=>{
+    console.log('跳出全局');
+});
+export default router;
