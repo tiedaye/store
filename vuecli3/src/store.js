9d53518f2b746d77f19dfbfd964b98d89d846dd7
zhangshuang-h <781792980@qq.com>
2018-08-15 23:11:58 +0800

    store

--- vuecli3/src/store.js
+++ vuecli3/src/store.js
@@ -5,12 +5,25 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+      count:15,
+      msg:'qqq'
   },
   mutations: {
-
+     add(state,num){
+       state.count +=num;
+     },
+      sub(state,num){
+          state.count -=num;
+      }
   },
   actions: {
-
+        addaction({ commit },num){
+            commit('add',num);
+        }
   },
+  getters:{
+      count100:(state)=>{
+        return state.count +100;
+      }
+  }
 });
