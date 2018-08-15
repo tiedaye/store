423ca304e21696850ea615cb8749ff2c1c911a63
zhangshuang-h <781792980@qq.com>
2018-08-15 23:11:01 +0800

    movie

---
+++ vuecli3/src/views/MovieDetail.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        电影详情页面
+        {{$route.params.id}}
+        <!--{{msg}}-->
+    </div>
+</template>
+<script>
+    export default {
+        // data(){
+        //     return {
+        //         msg:this.$route.params.id
+        //     }
+        // }
+    }
+</script>
+<style>
+
+</style>
