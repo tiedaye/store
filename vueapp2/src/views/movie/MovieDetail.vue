0157303c96b8cff864f5df96bb7376e410888bfa
zhangshuang-h <781792980@qq.com>
2018-08-17 16:47:48 +0800

    moviedetail

---
+++ vueapp2/src/views/movie/MovieDetail.vue
@@ -0,0 +1,35 @@
+<template>
+    <div v-if="show">
+        <h3>电影名称：{{MovieDetail.title}}</h3>
+        <div>
+            <img :src="MovieDetail.images.small" alt="">
+        </div>
+        <p>id:{{MovieDetail.id}}</p>
+        <p>简介：{{MovieDetail.summary}}</p>
+    </div>
+</template>
+
+<script>
+    import Axios from 'axios'
+    export default {
+        data(){
+            return{
+                MovieDetail:{},
+                show:false
+            }
+        },
+        name: "MovieDetail",
+        created(){
+            var movieId = this.$route.params.id;
+            Axios.get('https://bird.ioliu.cn/v1?url=https://api.douban.com/v2/movie/subject/'+movieId).then(res=>{
+                this.MovieDetail = res.data;
+                this.show = true;
+            });
+            console.log(this.MovieDetail);
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
