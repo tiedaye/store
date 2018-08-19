6bd4f6f9af4f926691993b77126281405c109ed3
zhangshuang-h <781792980@qq.com>
2018-08-19 21:08:17 +0800

    book

---
+++ vueapp2/src/views/book/Book.vue
@@ -0,0 +1,45 @@
+<template>
+    <swipe class="my-swipe">
+        <swipe-item class="slide1"></swipe-item>
+        <swipe-item class="slide2"></swipe-item>
+        <swipe-item class="slide3"></swipe-item>
+    </swipe>
+</template>
+
+<script>
+    import { Swipe, SwipeItem } from 'vue-swipe';
+    export default {
+        name: "Book",
+        components:{
+            swipe:Swipe,
+            'swipe-item':SwipeItem
+        }
+    }
+</script>
+
+<style scoped>
+    .my-swipe {
+        height: 200px;
+        color: #fff;
+        font-size: 30px;
+        text-align: center;
+    }
+
+    .slide1 {
+        background: url("http://localhost:8080/img/background1.jpg") center no-repeat;
+        background-size: contain;
+        color: #fff;
+    }
+
+    .slide2 {
+        background: url('http://localhost:8080/img/小丸子.jpeg') center no-repeat;
+        background-size: contain;
+        color: #000;
+    }
+
+    .slide3 {
+        background: url("http://localhost:8080/img/小奶熊.png") center no-repeat;
+        background-size: contain;
+        color: #fff;
+    }
+</style>
