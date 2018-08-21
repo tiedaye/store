67e91ad0d624e52e2e87f750ed2389e6ab55ad5a
zhangshuang-h <781792980@qq.com>
2018-08-21 21:06:33 +0800

    photo

---
+++ vueapp2/src/views/photo/Photo.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <router-link v-for="(val,index) in photoList" :to="'/photodetail/' + index">
+            <img  :src="val.src" alt="">
+        </router-link>
+
+    </div>
+</template>
+
+<script>
+    import Axios from 'axios';
+    export default {
+        data(){
+            return{
+                photoList:[]
+            }
+        },
+        name: "photo",
+        created(){
+            Axios.get('data/photodata.json').then(res=>{
+                this.photoList = res.data.photoData;
+            })
+
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    img{
+        width: 50%;
+    }
+</style>
