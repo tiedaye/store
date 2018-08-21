9ba9541d6495abad48bb3f532bc02ab10328d95f
zhangshuang-h <781792980@qq.com>
2018-08-21 21:07:20 +0800

    photodetail

---
+++ vueapp2/src/views/photo/PhotoDetail.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="photo">
+
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "PhotoDetail"
+    }
+</script>
+
+<style scoped>
+    .photo{
+        background-image: url('/img/1.jpg');
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-color: #000;
+        position:absolute;
+        top:0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+    }
+</style>
