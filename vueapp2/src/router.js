def6a83caed74e2e81f2fba067768331a2dbd5aa
zhangshuang-h <781792980@qq.com>
2018-08-21 21:08:45 +0800

    router

---
+++ vueapp2/src/router.js
@@ -0,0 +1,40 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Movie from './views/movie/Movie.vue';
+import Music from './views/music/Music.vue';
+import Book from './views/book/Book.vue';
+import Photo from './views/photo/Photo.vue';
+import MovieDetail from './views/movie/MovieDetail.vue';
+import PhotoDetail from './views/photo/PhotoDetail.vue';
+ Vue.use(Router);
+ let router = new Router({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '/movie',
+            component: Movie,
+        },
+        {
+            path: '/music',
+            component: Music,
+        },
+        {
+            path: '/book',
+            component: Book,
+        },
+        {
+            path: '/photo',
+            component: Photo,
+        },
+        {
+            path: '/moviedetail/:id',
+            component: MovieDetail,
+        },
+        {
+            path: '/photodetail',
+            component: PhotoDetail,
+        }
+    ],
+});
+export default router;
