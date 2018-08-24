06e5016098b7684dd8952e1fc33b200a9352a070
zhangshuang-h <781792980@qq.com>
2018-08-24 20:30:58 +0800

    mywebsite

--- mywebsite/website1.js
+++ mywebsite/website1.js
@@ -1,21 +1,32 @@
-var aimg = document.getElementsByTagName('img');
-var ocontainer = document.getElementById('container');
-var ali = ocontainer.getElementsByTagName('li')
-var zIndex = 4;
-var nowindex =0;
-setInterval(function () {
-
-    aimg[nowindex].style.zIndex = ++zIndex;
-    aimg[nowindex].style.opacity = 0;
-    animate(aimg[nowindex], {opacity: 100});
-    for(var j=0;j<ali.length;j++){
-        ali[j].style.background = ""
-    }
-    ali[nowindex].style.background = "hotpink";
-    nowindex++;
-    if(nowindex ==ali.length){
-        nowindex = 0;
-
-    }
-
-},1000);
+var onumber = document.getElementById('num');
+var oleft = document.getElementById('left');
+var oright = document.getElementById('right');
+var ogo = document.getElementById('goup');
+var speed =-2;
+oleft.onclick = function () {
+    speed=-2;
+};
+oright.onclick = function () {
+    speed=2;
+};
+    setInterval(function () {
+        var leftdis = onumber.offsetLeft;
+        onumber.style.left= leftdis + speed + 'px';
+        if(leftdis < -onumber.offsetWidth/2){
+            onumber.style.left = 0;
+        }
+        if(leftdis>0){
+            onumber.style.left=-onumber.offsetWidth/2 + 'px'
+        }
+    },30);
+    ogo.onclick = function(){
+    var top = document.documentElement.scrollTop;
+    goup.style.backgroundPosition = '-180px';
+    var timer = setInterval(function () {
+     scrollTo(0,top*=0.5);
+     if(top<1){
+         clearInterval(timer);
+         scrollTo(0,0);
+     }
+  },50)
+}
