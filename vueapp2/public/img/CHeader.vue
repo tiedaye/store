68eae7d6b5528b376725b8a1330c9a74821629f6
zhangshuang-h <781792980@qq.com>
2018-08-24 16:26:11 +0800

    小项目app2 电影 书籍 音乐 图片的完整版

---
+++ vueapp2/public/img/CHeader.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="header" :style="{background:bgColor}">
+        首页
+        <h1 class="header-title">{{title}}</h1>
+    </div>
+</template>
+<script>
+  import {mapState} from 'vuex'
+    export  default {
+      computed:mapState(['bgColor','title'])
+    }
+</script>
+<style lang="scss">
+   .header{
+       z-index: 1;
+       height:1rem;
+       width: 100%;
+       color: #fff;
+       line-height: 1rem;
+       position: fixed;
+       font-size: 16px;
+       top:0;
+       &-title{
+           position: absolute;
+           top: 50%;
+           left: 50%;
+           transform: translate(-50%,-50%);
+       }
+   }
+</style>
