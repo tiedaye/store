a8e0cc23eec3dbb2b70af0e073f9f8d16933adad
zhangshuang-h <781792980@qq.com>
2018-08-24 16:24:56 +0800

    cheader

---
+++ vueapp2/src/components/CHeader.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="header" :style="{background:Color}">
+        首页
+        <h1 class="header-title">{{tit}}</h1>
+    </div>
+</template>
+<script>
+  import {mapState} from 'vuex';
+
+    export  default{
+        props:['Color','tit'],
+      // computed:mapState(['bgColor','title'])
+    }
+</script>
+<style lang="scss">
+   .header{
+       z-index: 1;
+       height:1rem;
+       width: 100%;
+       color: #fff;
+       line-height: 1rem;
+       position: fixed;
+       font-size: 16px;
+       top:0;
+       &-title{
+           position: absolute;
+           top: 50%;
+           left: 50%;
+           transform: translate(-50%,-50%);
+       }
+   }
+</style>
