22fb00cfea30312dce189d25d823261ead7210e8
zhangshuang-h <781792980@qq.com>
2018-08-24 16:24:19 +0800

    store

--- vueapp2/src/store.js
+++ vueapp2/src/store.js
@@ -6,17 +6,24 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state:{
     bgColor:'rgb(33, 150, 243)',
-      title:'Movie'
+      title:'Movie',
+      photoList:[]
   },
   mutations:{
         changbg(state,menu){
            state.bgColor = menu.bgColor;
            state.title = menu.title;
-        }
+        },
+      photo(state,list){
+          state.photoList = list;
+      }
   },
   actions:{
         changbg({commit},menu){
            commit('changbg',menu)
-        }
+        },
+       setPhoto({commit},list){
+          commit('photo',list)
+      }
   },
 });
