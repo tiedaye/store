7a85651892039a41fe280609dcad41f1a1a74280
zhangshuang-h <781792980@qq.com>
2018-08-24 16:23:53 +0800

    main

--- vueapp2/src/main.js
+++ vueapp2/src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import VueTouch from 'vue-touch';
 Vue.config.productionTip = false;
+
 import '@/assets/css/reset.css';
 import '@/assets/js/rem.js';
 import "@/assets/css/vue-swipe.css";
+Vue.use(VueTouch,{name:'v-touch'});
 new Vue({
   router,
   store,
