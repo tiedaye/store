e11008d0d5aa1bfa3db71a77c8ffa8510ca14ba3
zhangshuang-h <781792980@qq.com>
2018-09-17 21:48:17 +0800

    app

---
+++ reactApp/react-app/src/component/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { connect }from "react-redux"
+import store from "../store";
+const act = (num)=>{
+  return {
+    type:"SUB",
+    text:num
+  }
+};
+class App extends Component {
+  sub(num){
+    store.dispatch(act(num));
+  }
+  render(){
+    return (
+        <div>
+            <h1>{this.props.count}</h1>
+            <button onClick={()=>{
+                  this.props.act(3);
+               }
+          }>sub</button>
+       <button onClick={this.sub.bind(this,2)}>sub</button>
+        </div>
+    );
+  }
+}
+const mapStateToProps =(state) =>{
+  return{
+      count:state.count
+  }
+};
+//或者
+// const mapStateToProps =(state) =>({
+//         count:state.count
+// });
+const mapDispatchToProps = (dispatch)=>{
+         return{
+           act:(xx)=>{dispatch(act(xx))}
+         }
+};
+export default connect (mapStateToProps,mapDispatchToProps)(App);
