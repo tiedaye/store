3bbb424793d5f69061b3dffd7e50ad22c9ec4be6
zhangshuang-h <781792980@qq.com>
2018-09-18 21:54:28 +0800

    home

---
+++ reactApp/react-app/src/component/Home.js
@@ -0,0 +1,11 @@
+import React,{ Component } from "react";
+class Home extends Component{
+    render(){
+        return (
+            <div>
+                我是主页
+            </div>
+        )
+    }
+}
+export default Home;
