042df46c587ef6f8f075a8960495faf225dbd76a
zhangshuang-h <781792980@qq.com>
2018-09-18 21:57:32 +0800

    index.js

--- reactApp/react-app/src/index.js
+++ reactApp/react-app/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './component/App';
-import { Provider } from "react-redux";
-import store from "./store"
+//import App from './component/App';
+//import { Provider } from "react-redux";
+//import store from "./store"
+import Main from "./component/Main"
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    <Main> </Main>,
     document.getElementById('root'));
