588f2e5c00677f24bf2576214f929b6ab1677c17
zhangshuang-h <781792980@qq.com>
2018-09-18 21:56:56 +0800

    pageB

---
+++ reactApp/react-app/src/component/PageB.js
@@ -0,0 +1,11 @@
+import React,{ Component } from "react";
+class PageB extends Component{
+    render(){
+        return (
+            <div>
+               我是B页面
+            </div>
+        )
+    }
+}
+export default PageB;
