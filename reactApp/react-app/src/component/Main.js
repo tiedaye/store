bf1f8934d953491bbae6f3fe69c4cb6dba102e28
zhangshuang-h <781792980@qq.com>
2018-09-20 20:33:00 +0800

    react-app main

--- reactApp/react-app/src/component/Main.js
+++ reactApp/react-app/src/component/Main.js
@@ -1,25 +1,59 @@
 import React,{ Component } from "react";
-import	{ BrowserRouter as Router, Route ,Link,Switch } from "react-router-dom"
+import	{ BrowserRouter as Router, Route ,Link,Switch,Redirect,Prompt,NavLink } from "react-router-dom"
 import PageA from "./PageA";
 import PageB from "./PageB";
 import Home from "./Home";
 import Children from "./Children";
+// const Jump = (props)=>{
+//     return <Link to={props.to}>Jump</Link>
+// };
+const web = (props)=>{
+    return (
+            <div>
+                <button onClick={()=>{
+                    props.history.push("/bb")
+                }}>click</button>
+            </div>
+        )
+
+ };
+class Jump extends  Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return (
+            <Link to={this.props.to}>Jump</Link>
+        )
+    }
+}
 class Main extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            show:true
+        }
+    }
     render(){
         return (
             <div>
                 main主页面
                 <Router>
                     <div>
-                        <Link to="/">跳转Home</Link>
+                        <NavLink activeClassName="selected"
+                                 activeStyle={{color:"red"}} to="/">跳转Home</NavLink>
                         --------
-                        <Link to="/aa/4">跳转A</Link>
+                        <NavLink to="/aa/4">跳转A</NavLink>
                         --------
-                        <Link to="/bb">跳转B</Link>
+                        <NavLink to="/bb">跳转B</NavLink>
                         --------
-                        <Link to="/render">跳转render</Link>
+                        <NavLink activeClassName="selected" isActive={(props)=>{
+                            console.log(props)
+                             }} to="/render">跳转render</NavLink>
                         --------
-                        <Link to="/children/2">跳转child</Link>
+                        <NavLink to="/children/2">跳转child</NavLink>
+                        --------
+                        <NavLink to="/cc">编程式导航</NavLink>
                         {/*<Switch>*/}
                             {/*<Route path="/aa" component={PageA}></Route>*/}
                             {/*<Route path="/bb" component={PageB}></Route>*/}
@@ -29,9 +63,13 @@ class Main extends Component{
                         <Route path="/aa/:id" component={PageA}></Route>
                         <Route path="/bb" component={PageB}></Route>
                         <Route path="/render" render={()=>{
-                            return <h1>我是render页面</h1>
+                            // return <h1>我是render页面</h1>
+                           return <Redirect to="/bb"></Redirect>
                         }}></Route>
                         <Route path="/children/:num" children={Children}></Route>
+                        <Jump to="/"></Jump>
+                        <Route path="/cc" component={web}></Route>
+                        <Prompt when={this.state.show} message={"是否离开当前页面"}></Prompt>
                     </div>
                 </Router>
             </div>
