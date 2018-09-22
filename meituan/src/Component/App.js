08151eabd10bd69f7feb9bbfdd4595e2d4674aed
zhangshuang-h <781792980@qq.com>
2018-09-22 21:47:32 +0800

    app js

---
+++ meituan/src/Component/App.js
@@ -0,0 +1,18 @@
+import React,{ Component } from "react"
+import { BrowserRouter as Router,Route } from 'react-router-dom';
+import Home from './Home';
+import Detail from './Detail';
+export default class App extends Component{
+    render(){
+        return(
+            <div>
+                <Router>
+                    <div>
+                        <Route exact path="/" component={Home}></Route>
+                        <Route path="/detail" component={Detail}></Route>
+                    </div>
+                </Router>
+            </div>
+        )
+    }
+}
