da996beaf5298a47db32d59e28c4da10d6a52b41
zhangshuang-h <781792980@qq.com>
2018-09-27 21:05:54 +0800

    index  js

--- meituan/src/index.js
+++ meituan/src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./Component/App"
+import "./asset/css/reset.css"
+import "./asset/js/rem"
 ReactDOM.render(<App/>, document.getElementById('root'));
